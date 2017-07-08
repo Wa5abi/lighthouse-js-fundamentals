b714b6cdcdec4bb5639ef8556318e03f8478dea9
Dene <W4548147gm411@gmail.com>
2017-07-08 10:46:38 +0000

    Adds ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+
+while (repeat < ingredients.length) {
+  console.log(ingredients[repeat]);
+
+  repeat++;
+}
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (var backwards = ingredients.length - 1; backwards >= 0; backwards--) {
+  console.log(ingredients[backwards])
+}
